163ec4619404eb639ba7fb58561801a489314440
Oliver Christensen <orc13a@live.dk>
2021-09-22 14:19:06 +0200

    comments

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 // 1 : https://nodejs.org/api/dgram.html
 // 2 : https://gist.github.com/sid24rane/6e6698e93360f2694e310dd347a2e2eb
+// 3 : https://github.com/sindresorhus/boxen#boxen
 
 // ########################################
 
-const udp = require('dgram');
-const boxen = require('boxen');
+const udp = require('dgram'); // En del af Node.js, kilde 1
+const boxen = require('boxen'); // Til at lave kasserne i terminalen, kilde 3
 
 const PORT = 8888;
 const HOST = 'localhost';
@@ -15,6 +16,7 @@ const HOST = 'localhost';
 
 const server = udp.createSocket('udp4');
 
+// Når serveren modtager en besked
 server.on('message', function(msg, info){
     // 'Received %d bytes from %s:%d', msg.length, info.address, info.port
 
@@ -23,6 +25,7 @@ server.on('message', function(msg, info){
     , { padding: 1, borderStyle: 'round', borderColor: 'green' }));
 });
 
+// Serverens detaler 
 server.on('listening', function(){
     let address = server.address();
     let port = address.port;
@@ -36,16 +39,19 @@ server.on('listening', function(){
     , { padding: 1, borderStyle: 'round', borderColor: 'yellow' }));
 });
 
+// Hvis serveren slukkes
 server.on('close', function(){
     console.log('Socket is closed !');
 });
 
+// Start serven, bruger "listening" funktionen
 server.bind(PORT);
 
 // Client ########################################
 
 const client = udp.createSocket('udp4');
 
+// Client sender en besked til serveren
 client.send('hej', PORT, HOST, function(error){
     if(error){
         client.close();
